ac0a16eda6ed019eaa21904594bd6838c8fb9487
navadeep-chan <navadeepchandran06@gmail.com>
2025-04-25 11:27:14 +0530

    Update app_development.py
    update-4, changed file path format from windows to (os.path.join()).

--- app_development.py
+++ app_development.py
@@ -17,7 +17,7 @@ def pdf_construction(c_name, s_name, place, description, image_local_path):
     pdf.rect(margin, margin, pdf.w - 2*margin, pdf.h - 2*margin)
     
     #logo
-    logo_path = "images\\mssrf_logo.png"
+    logo_path = os.path.join("images", "mssrf_logo.png")
     logo_width = 50
     x_position = (pdf.w - logo_width)/2
     pdf.image(logo_path, x = x_position, y = 10, w = logo_width)
@@ -108,7 +108,8 @@ def name_board_generation(common_name, scientific_name, link_of_pdf):
     qr_link = link_of_pdf
     
     # Load base background image
-    background = Image.open("images\\bg_image.png").convert('RGBA')
+    background_image_path = os.path.join('images', 'bg_image.png')
+    background = Image.open(background_image_path).convert('RGBA')
     
     # Generate QR code
     qr_image = qrcodelink(qr_link)
@@ -122,8 +123,10 @@ def name_board_generation(common_name, scientific_name, link_of_pdf):
     draw = ImageDraw.Draw(background)
     
     # Load fonts (adjust the path if needed)
-    common_font = ImageFont.truetype("Fonts\\arialbd.ttf", 100)  # Arial Bold, size 60
-    scientific_font = ImageFont.truetype("Fonts\\ariali.ttf", 60)  # Arial Italic, size 40
+    common_font_path = os.path.join('Fonts', 'arialbd.ttf')
+    scientific_font_path = os.path.join('Fonts', 'ariali.ttf')
+    common_font = ImageFont.truetype(common_font_path, 100)  # Arial Bold, size 60
+    scientific_font = ImageFont.truetype(scientific_font_path, 60)  # Arial Italic, size 40
     
     
     # Add Common Name text
